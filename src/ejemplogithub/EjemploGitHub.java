38559f7b37b057ecdf659b442449678caf4a430c
Carlos Cardoso <Carlos Cardoso@CARLOS-94.mshome.net>
2018-10-09 23:38:26 -0500

    primera subida

---
+++ src/ejemplogithub/EjemploGitHub.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogithub;
+
+/**
+ *
+ * @author Carlos Cardoso
+ */
+public class EjemploGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        String nombre;
+        String apellido;
+        String sexo;
+        int edad;
+
+        Persona persona = new Persona();
+
+        persona.setNombre("Carlos");
+        persona.setApellido("Cardoso");
+        persona.setEdad(23);
+        persona.setSexo("M");
+
+        nombre = persona.getNombre();
+        apellido = persona.getApellido();
+        edad = persona.getEdad();
+        sexo = persona.getSexo();
+
+        System.out.println(nombre+" "+apellido+" edad: "+edad+" sexo: "+sexo);
+
+    }
+
+}
---
+++ src/ejemplogithub/Persona.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogithub;
+
+/**
+ *
+ * @author Carlos Cardoso
+ */
+public class Persona {
+    
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String sexo;
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+   
+    
+    
+    
+}
