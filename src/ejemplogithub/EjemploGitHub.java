ff12ba692b7fd8c13824917a0c22eb9928bcf817
Carlos Cardoso <Carlos Cardoso@CARLOS-94.mshome.net>
2018-10-10 00:22:49 -0500

    version 1.002

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -20,6 +20,10 @@ public class EjemploGitHub {
         String apellido;
         String sexo;
         int edad;
+        String nombre1;
+        String apellido1;
+        String sexo1;
+        int edad1;
 
         Persona persona = new Persona();
 
@@ -27,14 +31,26 @@ public class EjemploGitHub {
         persona.setApellido("Cardoso");
         persona.setEdad(23);
         persona.setSexo("M");
-
+        
         nombre = persona.getNombre();
         apellido = persona.getApellido();
         edad = persona.getEdad();
         sexo = persona.getSexo();
+        
+        Persona persona1 = new Persona();
+
+        persona1.setNombre("Paula");
+        persona1.setApellido("Abad");
+        persona1.setEdad(10);
+        persona1.setSexo("F");
+        nombre1 = persona1.getNombre();
+        apellido1 = persona1.getApellido();
+        edad1 = persona1.getEdad();
+        sexo1 = persona1.getSexo();
         System.out.println("");
         System.out.println("DATOS");
         System.out.println(nombre+" "+apellido+" edad: "+edad+" sexo: "+sexo);
+        System.out.println(nombre1+" "+apellido1+" edad: "+edad1+" sexo: "+sexo1);
 
     }
 
